644aae42e9ba2f4141f02c4127da1a667fcc6f60
Vincent Marcus <boyplankton@gmail.com>
2023-04-05 16:03:00 -0600

    removed returnTLSVersion

--- tls.go
+++ tls.go
@@ -10,23 +10,6 @@ import (
 	"time"
 )
 
-func returnTLSVersion(version uint16) string {
-	switch version {
-	case tls.VersionSSL30:
-		return "SSL 3.0"
-	case tls.VersionTLS10:
-		return "TLS 1.0"
-	case tls.VersionTLS11:
-		return "TLS 1.1"
-	case tls.VersionTLS12:
-		return "TLS 1.2"
-	case tls.VersionTLS13:
-		return "TLS 1.3"
-	}
-
-	return "Unknown"
-}
-
 func returnTLSCurve(curve tls.CurveID) string {
 	switch curve {
 	case tls.CurveP256:
